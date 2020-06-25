eaa50da2418709d406a283e6dc8d9f10405a2377
Daniil090901 <89320sport@mail.ru>
2020-06-25 15:00:46 +0300

    add project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("c=");
+            double c = double.Parse(Console.ReadLine());
+            Class1 kv2 = new Class1(a, b, c);
+            kv2.Decision();
+            Console.WriteLine(kv2.ToString());
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 0, b = 0, c = 0;
+            string expResult = "любое число";
+            Class1 kv = new Class1(a, b, c);
+            kv.Decision();
+            string actResult = kv.ToString();
+            Assert.AreEqual(expResult, actResult);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double a = 1, b = 1, c = 1;
+            string expResult = "нет корней";
+            Class1 kv = new Class1(a, b, c);
+            kv.Decision();
+            string actResult = kv.ToString();
+            Assert.AreEqual(expResult, actResult);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double a = 1, b = 1, c = -2;
+            string expResult = "x1=1   x2=-2";
+            Class1 kv = new Class1(a, b, c);
+            kv.Decision();
+            string actResult = kv.ToString();
+            Assert.AreEqual(expResult, actResult);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double a = 0, b = 1, c = -1;
+            string expResult = "x=1";
+            Class1 kv = new Class1(a, b, c);
+            kv.Decision();
+            string actResult = kv.ToString();
+            Assert.AreEqual(expResult, actResult);
+        }
+    }
+}
